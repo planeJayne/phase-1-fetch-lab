4c5ca9bda488cf73455d0b9e16341439cf0fb13e
Jasmin <jasmin.parish@outlook.com>
2022-07-28 23:46:03 -0400

    commit tree 2e20ddc01eaca2826ae30228983a56eaf8d5404c

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+
+});
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(books => (renderBooks(books))
+   
   
-}
+)}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
+    console.log(book)
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    h2.textContent = book.name
+    main.append(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
